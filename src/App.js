d34be91d773a082770d5a238212aa47b318ab540
hariake <tosarv@gmail.com>
2024-02-13 21:28:15 +0200

    uued asjad ja nende sorteerimine

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import NewExpense from './components/NewExpense/NewExpense';
 
 
 const App = () =>{
+    const DYMMY_EXPENSES =[
+        {
+            id: 'id1',
+            date: new Date(2023, 9 ,6),
+            title: 'New Book',
+            amount: 30.99
+        },
+        {
+            id: 'id2',
+            date: new Date(2023, 9 ,6),
+            title: 'New Jeans',
+            amount: 99.99
+        },
+        {
+            id: 'id3',
+            date: new Date(2024, 2 ,25),
+            title: 'New Bag',
+            amount: 139.99
+        }
+    ]
   const addExpenseHandler = (expense) =>{
     console.log('In App.js')
     console.log(expense)
@@ -11,7 +31,7 @@ const App = () =>{
   return (
     <div className="App">
         <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-        <Expenses expenses={Expenses}/>
+        <Expenses expenses={DYMMY_EXPENSES}/>
       </div>
   )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,34 +2,26 @@ import "./Expenses.css"
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
-const Expenses = () =>{
-    const expenses =[
-    {
-      date: new Date(2023, 9 ,6),
-      title: 'New Book',
-      price: 30.99
-    },
-  {
-    date: new Date(2023, 9 ,6),
-    title: 'New Jeans',
-    price: 99.99
-  }
-  ]
+
+
+const Expenses = (props) =>{
   const saveSelectedYearHandler = (selectedYear) => {
     console.log(selectedYear)
   }
+  props.expenses.map((expense) => {
+      console.log(expense)
+  })
 
     return (
       <Card className='expenses'>
           <ExpensesFilter
             saveSelectedYear = {saveSelectedYearHandler} 
           />
-          <ExpenseItem
-          expenseData = {expenses[0]}
-          />
-          <ExpenseItem
-          expenseData = {expenses[1]}
-          />
+          {
+              props.expenses.map((expense) => {
+                  return <ExpenseItem expenseData={expense} key={expense.id}/>
+              })
+          }
       </Card>
   )
   }
