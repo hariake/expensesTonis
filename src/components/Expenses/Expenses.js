580d94a2eabcade2919356f5ace06e14903b6046
hariake <tosarv@gmail.com>
2024-02-20 19:56:22 +0200

    8 tundi läks aega

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,23 +2,28 @@ import "./Expenses.css"
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 
 const Expenses = (props) =>{
+    const [filteredYear, setFilteredYear] =useState("")
   const saveSelectedYearHandler = (selectedYear) => {
-    console.log(selectedYear)
+        setFilteredYear(selectedYear)
+    console.log("The selected year is " + selectedYear);
   }
-  props.expenses.map((expense) => {
-      console.log(expense)
-  })
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+      return expense.date.getFullYear().toString() === filteredYear
+  });
+
 
     return (
       <Card className='expenses'>
           <ExpensesFilter
-            saveSelectedYear = {saveSelectedYearHandler} 
+            saveSelectedYear = {saveSelectedYearHandler}
           />
           {
-              props.expenses.map((expense) => {
+              filteredExpenses.map((expense) => {
                   return <ExpenseItem expenseData={expense} key={expense.id}/>
               })
           }
