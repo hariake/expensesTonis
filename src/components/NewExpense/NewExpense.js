cd0f3a53cb6a8bbff18ab1aaa4e59b6c6c4f18bf
hariake <tosarv@gmail.com>
2024-02-29 20:16:35 +0200

    Formi kinni panek ja lahti tegemine nii formi saates kui cancel vajutades

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,39 @@
+import { useState } from "react"
 import ExpenseForm from "./ExpenseForm"
 import "./NewExpense.css"
 
+
 const NewExpense = (props) =>{
+    const [editForm, setEditForm] =useState(false);
+    
     const saveExpenseDatahandler = (enteredExpenseData) =>{
         const expenseData ={
             ...enteredExpenseData,
             id: Math.random().toString()
         } 
         props.onAddExpense(expenseData)
-    } 
-    return(
+        setEditForm(false);
+    }; 
+    const cancelEditingHandler = () => {
+        setEditForm(false);
+    };
+    return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDatahandler}/>
+            {!editForm && (
+                <button onClick={() => setEditForm(true)}>Add New Expense</button>
+            )}
+
+            {editForm && (
+                <div>
+                    <ExpenseForm
+                        onSaveExpenseData={saveExpenseDatahandler}
+                        onCancel={cancelEditingHandler}
+                    />
+                    <button onClick={cancelEditingHandler}>Cancel</button>
+                </div>
+            )}
         </div>
-    )
+    );
 } 
 
-export default NewExpense
+export default NewExpense;
